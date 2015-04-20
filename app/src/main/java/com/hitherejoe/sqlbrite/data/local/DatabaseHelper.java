37374e8df98b41f27ab25f9c9b79af983be9a371
Joe Birch <joe@ribot.co.uk>
2015-04-20 12:29:27 +0100

    Code tidy-up

--- app/src/main/java/com/hitherejoe/sqlbrite/data/local/DatabaseHelper.java
+++ app/src/main/java/com/hitherejoe/sqlbrite/data/local/DatabaseHelper.java
@@ -3,20 +3,17 @@ package com.hitherejoe.sqlbrite.data.local;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
-import com.hitherejoe.sqlbrite.data.local.util.OnSubscribeCursor;
 import com.hitherejoe.sqlbrite.data.model.Person;
-import com.hitherejoe.sqlbrite.ui.util.OperatorWhileDoWhile;
 import com.squareup.sqlbrite.SqlBrite;
+import com.squareup.sqlbrite.SqlBrite.Query;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
 import rx.Subscriber;
-import rx.functions.Action1;
-import rx.functions.Func0;
+import rx.android.content.ContentObservable;
 import rx.functions.Func1;
 
 public class DatabaseHelper {
@@ -52,16 +49,10 @@ public class DatabaseHelper {
 
     public Observable<Person> getAllPeople() {
         return mSqlBrite.createQuery(Db.PersonTable.TABLE_NAME, "SELECT * FROM " + Db.PersonTable.TABLE_NAME)
-                .map(new Func1<SqlBrite.Query, Cursor>() {
+                .flatMap(new Func1<Query, Observable<Person>>() {
                     @Override
-                    public Cursor call(SqlBrite.Query query) {
-                        return query.run();
-                    }
-                })
-                .flatMap(new Func1<Cursor, Observable<Person>>() {
-                    @Override
-                    public Observable<Person> call(Cursor cursor) {
-                        return Observable.create(new OnSubscribeCursor(cursor))
+                    public Observable<Person> call(Query query) {
+                        return ContentObservable.fromCursor(query.run())
                                 .map(new Func1<Cursor, Person>() {
                                     @Override
                                     public Person call(Cursor cursor) {
